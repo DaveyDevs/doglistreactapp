c00185347973af5ae5f884b6c0a9952ce3e0a6cd
Davey Devs <45680621+DaveyDevs@users.noreply.github.com>
2021-02-15 21:41:26 -0600

    hash router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./css/App.css";
 import AppContext from "./AppContext";
